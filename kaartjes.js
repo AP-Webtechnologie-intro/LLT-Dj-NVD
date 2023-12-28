1cb362fd821cd4d8a68cfb070b538a1f7de5aba1
Dj-NVD <88896089+Dj-NVD@users.noreply.github.com>
2023-12-28 11:58:18 +0100

    2 pagina's + kaarten
    2 paginas aangemaakt, van netflix en disney+, ook javascript aangemaakt voor de kaarten die erin zitten

---
+++ kaartjes.js
@@ -0,0 +1,56 @@
+//hieronder de kaart voor disneyland
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('dlp-map').setView([48.87262, 2.77677], 17);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap-bijdragers'
+    }).addTo(map);
+
+    var marker1 = L.marker([48.87262, 2.77677]).addTo(map)
+        .bindPopup('<div style="text-align: center;">Disneyland Parijs<br>Magische plek waar we deze zomer zijn geweest!</div>');
+
+    var marker2 = L.marker([48.86635, 2.77855]).addTo(map)
+        .bindPopup('<div style="text-align: center;">Tower of Terror<br>Hier zijn we toch echt te vaak in geweest hoor</div>');
+
+    var marker3 = L.marker([48.83463, 2.82141]).addTo(map)
+        .bindPopup('<div style="text-align: center;">Davy Crockett Ranch<br>goed gevonden! hier hebben we overnacht tijdens het verblijf!</div>');
+
+    var centralCoordinates = [48.87262, 2.77677];
+    map.setView(centralCoordinates, 16);
+    marker1.openPopup();
+});
+
+
+//hieronder de kaart voor de netflix pagina
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('netflix-map').setView([37.25658, -121.96426], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap-bijdragers'
+    }).addTo(map);
+
+    L.marker([37.25761, -121.96336]).addTo(map)
+    .bindPopup('<div style="text-align: center;">Gebouw D</div>')
+    L.marker([37.25709, -121.96408]).addTo(map)
+    .bindPopup('<div style="text-align: center;">Gebouw E<br>De hoofdingang</div>')
+    .openPopup();
+    L.marker([37.25609, -121.96454]).addTo(map)
+    .bindPopup('<div style="text-align: center;">Gebouw F</div>')
+    L.marker([37.25543, -121.96501]).addTo(map)
+    .bindPopup('<div style="text-align: center;">Gebouw G</div>')
+
+});
+
+//hieronder de kaart voor telenet
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('telenet-map').setView([0.000, 0.000], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap-bijdragers'
+    }).addTo(map);
+
+    L.marker([0.000, 0.000]).addTo(map)
+    .bindPopup('<div style="text-align: center;">Disneyland Parijs<br>Magische plek waar we deze zomer zijn geweest!</div>')
+    .openPopup();
+});
+
+
